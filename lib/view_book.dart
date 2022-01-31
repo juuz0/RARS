ad0bd4553c22a270c872241faaa723284f7b8f89
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2022-01-31 13:56:26 +0530

    Increase quality of PDFs
    native_pdf_view has a bug (still open) related to PDF qualities. We cant
    do much there but this increases the zoom size of page and some
    rendering dimensions, which looks better than earlier

--- lib/view_book.dart
+++ lib/view_book.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: camel_case_types
-
 import 'package:flutter/material.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 import 'package:rars/book.dart';
@@ -9,14 +7,13 @@ import 'package:rars/tabs.dart';
 class ViewBook extends StatefulWidget {
   final List<Book> tabListHere;
   final Book book;
-  const ViewBook(this.tabListHere,this.book, {Key? key}) : super(key: key);
+  const ViewBook(this.tabListHere, this.book, {Key? key}) : super(key: key);
 
   @override
-  _viewBookState createState() => _viewBookState();
+  _ViewBookState createState() => _ViewBookState();
 }
 
-class _viewBookState extends State<ViewBook> {
-  
+class _ViewBookState extends State<ViewBook> {
   static const int _initialPage = 1;
   int _actualPageNumber = _initialPage, _allPagesCount = 0;
   bool isSampleDoc = true;
@@ -37,111 +34,130 @@ class _viewBookState extends State<ViewBook> {
     _pdfController.dispose();
     super.dispose();
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
+      body: Column(
         children: [
-          AppBar(
-            title: Text(widget.book.title,
-              style: const TextStyle(color: Colors.blue),
-            ),
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            color: Colors.blue,
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
-          ),),
-            
-            actions: <Widget>[
-              IconButton(
-              icon: const Icon(Icons.navigate_before_outlined),
-              color: Colors.amber,
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MainScreen()),
-              ),),
-              IconButton(
-                color: Colors.blue,
-                icon: const Icon(Icons.navigate_before),
-                onPressed: () {
-                  _pdfController.previousPage(
-                    curve: Curves.ease,
-                    duration: const Duration(milliseconds: 100),
-                  );
-                },
+          Expanded(
+            flex: 1,
+            child: AppBar(
+              title: Text(
+                widget.book.title,
+                style: const TextStyle(color: Colors.blue),
               ),
-              Container(
-                alignment: Alignment.center,
-                child: Text(
-                  '$_actualPageNumber/$_allPagesCount',
-                  style: const TextStyle(fontSize: 22, color: Colors.blue,),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              leading: Builder(
+                builder: (context) => IconButton(
+                  icon: const Icon(Icons.menu),
+                  color: Colors.blue,
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
                 ),
               ),
-              IconButton(
-                color: Colors.blue,
-                icon: const Icon(Icons.navigate_next),
-                onPressed: () {
-                  _pdfController.nextPage(
-                    curve: Curves.ease,
-                    duration: const Duration(milliseconds: 100),
-                  );
-                },
-              ),
-              IconButton(
-                color: Colors.amber,
-                icon: const Icon(Icons.refresh),
-                onPressed: () {
-                  if (isSampleDoc) {
-                    _pdfController.loadDocument(
-                        PdfDocument.openAsset('assets/images/sample.pdf'));
-                  } else {
-                    _pdfController.loadDocument(
-                        PdfDocument.openAsset('assets/images/sample.pdf'));
-                  }
-                  isSampleDoc = !isSampleDoc;
-                },
-              )
-            ],
-          ),
-        SizedBox(
-          height: 500,
-          width: 1000,
-          child: PdfView(
-              scrollDirection: Axis.vertical,
-              documentLoader: const Center(child: CircularProgressIndicator()),
-              pageLoader: const Center(child: CircularProgressIndicator()),
-              controller: _pdfController,
-              onDocumentLoaded: (document) {
-                setState(() {
-                  _allPagesCount = document.pagesCount;
-                });
-              },
-              onPageChanged: (page) {
-                setState(() {
-                  _actualPageNumber = page;
-                });
-              },
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.navigate_before_outlined),
+                  color: Colors.amber,
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainScreen()),
+                  ),
+                ),
+                IconButton(
+                  color: Colors.blue,
+                  icon: const Icon(Icons.navigate_before),
+                  onPressed: () {
+                    _pdfController.previousPage(
+                      curve: Curves.ease,
+                      duration: const Duration(milliseconds: 100),
+                    );
+                  },
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    '$_actualPageNumber/$_allPagesCount',
+                    style: const TextStyle(
+                      fontSize: 22,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  color: Colors.blue,
+                  icon: const Icon(Icons.navigate_next),
+                  onPressed: () {
+                    _pdfController.nextPage(
+                      curve: Curves.ease,
+                      duration: const Duration(milliseconds: 100),
+                    );
+                  },
+                ),
+                IconButton(
+                  color: Colors.amber,
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () {
+                    if (isSampleDoc) {
+                      _pdfController.loadDocument(
+                          PdfDocument.openAsset('assets/images/sample.pdf'));
+                    } else {
+                      _pdfController.loadDocument(
+                          PdfDocument.openAsset('assets/images/sample.pdf'));
+                    }
+                    isSampleDoc = !isSampleDoc;
+                  },
+                )
+              ],
             ),
-        ),
-
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(onPressed: ()=>{ setState((){
-              Brightness.dark;
-            })}, icon: const Icon(Icons.bookmark)),
-            IconButton(onPressed: ()=>{}, icon: const Icon(Icons.favorite)),
-            IconButton(onPressed: ()=>{ }, icon: const Icon(Icons.format_paint_rounded))
-          ],
-        )
- 
+          ),
+          Expanded(
+            flex: 8,
+            child: SizedBox(
+              height: 2000,
+              width: 6000,
+              child: PdfView(
+                scrollDirection: Axis.vertical,
+                documentLoader:
+                    const Center(child: CircularProgressIndicator()),
+                pageLoader: const Center(child: CircularProgressIndicator()),
+                controller: _pdfController,
+                onDocumentLoaded: (document) {
+                  setState(() {
+                    _allPagesCount = document.pagesCount;
+                  });
+                },
+                onPageChanged: (page) {
+                  setState(() {
+                    _actualPageNumber = page;
+                  });
+                },
+                renderer: (PdfPage page) => page.render(
+                  width: page.width * 2,
+                  height: page.height * 2,
+                  format: PdfPageFormat.JPEG,
+                ),
+                pageBuilder: (
+                  Future<PdfPageImage> pageImage,
+                  int index,
+                  PdfDocument document,
+                ) =>
+                    PhotoViewGalleryPageOptions(
+                  imageProvider: PdfPageImageProvider(
+                    pageImage,
+                    index,
+                    document.id,
+                  ),
+                  minScale: PhotoViewComputedScale.contained * 1.5,
+                  maxScale: PhotoViewComputedScale.contained * 4.0,
+                  initialScale: PhotoViewComputedScale.contained * 1.5,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
       drawer: Tabs(widget.tabListHere),
